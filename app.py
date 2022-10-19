aa544e64977ff850b0766c64a9e01c5636ea855d
Bennett Bierman <bennettbierman@Bennetts-MacBook-Pro.local>
2022-10-18 20:24:51 -0400

    started SSV Ranker

---
+++ app.py
@@ -0,0 +1,11 @@
+import tkinter as tk
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Tkinter MVC Demo')
+
+
+if __name__ == '__main__':
+    app = App()
+    app.mainloop()
